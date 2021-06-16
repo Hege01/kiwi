cdb4d9c94468ad444aecddb695a29c263b3432a7
marek <marek@Heges-Envy-x360>
2021-06-16 19:33:44 +0200

    kiwi_task

---
+++ hege_eclipse.zip_expanded/kiwi_task_project/src/com/sun/tools/javac/util/Assert.java
@@ -0,0 +1,10 @@
+package com.sun.tools.javac.util;
+
+public class Assert {
+
+	public static void assertEquals(String attribute, String string, String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
